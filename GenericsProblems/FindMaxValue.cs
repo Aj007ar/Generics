e3273557ead74cdec19cae136b5865c75ecfb1f1
Aj007ar <aj007.ar@gmail.com>
2022-08-23 14:05:54 +0530

    [Ajay] Added: UC2 for Generic problems

--- GenericsProblems/FindMaxValue.cs
+++ GenericsProblems/FindMaxValue.cs
@@ -14,8 +14,14 @@ namespace GenericsProblems
             int max = number1.CompareTo(number2) == 1 ? number1 : number2;
             max = max.CompareTo(number3) == 1 ? max : number3;
             Console.WriteLine("The maximum number is {0}", max);
-            Console.WriteLine("\n");
             return max;
+        }
+        public static float MaxFloatValue(float number1, float number2, float number3)
+        {
+            float maxFloat = number1.CompareTo(number2) == 1 ? number1 : number2;
+            maxFloat = maxFloat.CompareTo(number3) == 1 ? maxFloat : number3;
+            Console.WriteLine("Maximum number is {0}",maxFloat);
+            return maxFloat;
         }
     }
 }
--- GenericsProblems/Program.cs
+++ GenericsProblems/Program.cs
@@ -7,6 +7,8 @@
             Console.WriteLine("WELCOME TO GENERIC PROBLEMS");
             Console.WriteLine("---------------> Max Integer <---------------");
             int max = FindMaxValue.MaxIntValue(45, 25, 55);
+            Console.WriteLine("\n----------------> Max Float Value <--------------");
+            float maxFloat = FindMaxValue.MaxFloatValue(14.2f, 15.2f, 43.2f);
         }
     }
 }
