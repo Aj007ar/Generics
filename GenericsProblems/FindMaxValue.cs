70890d7adbbcbb7f59b2d6098d7396b00c9d7c04
Aj007ar <aj007.ar@gmail.com>
2022-08-23 11:12:04 +0530

    [Ajay] Added : UC1 generic problems

---
+++ GenericsProblems/FindMaxValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblems
+{
+    internal class FindMaxValue
+    {
+        public static int MaxIntValue(int number1, int number2, int number3)
+        {
+
+            int max = number1.CompareTo(number2) == 1 ? number1 : number2;
+            max = max.CompareTo(number3) == 1 ? max : number3;
+            Console.WriteLine("The maximum number is {0}", max);
+            Console.WriteLine("\n");
+            return max;
+        }
+    }
+}
--- GenericsProblems/Program.cs
+++ GenericsProblems/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO GENERIC PROBLEMS");
+            Console.WriteLine("---------------> Max Integer <---------------");
+            int max = FindMaxValue.MaxIntValue(45, 25, 55);
         }
     }
 }
